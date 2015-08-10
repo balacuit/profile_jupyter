7325254824d645bc89b03d9071f6a7c0708c191e
Carlos Balacuit <Carlos.Balacuit@jpl.nasa.gov>
2015-08-10 14:35:35 -0700

    separated auto_numbering code;

--- static/custom/custom.js
+++ static/custom/custom.js
@@ -80,24 +80,4 @@
  * @class customjs
  * @static
  */
-/*
- Auto number headings.
-
- Src:  https://github.com/ipython/ipython/issues/4097
- */
-/*
-$(document).ready(function(){
-    $("h1").each(function(index1) {
-        $(this).prepend((index1+1) + ". ");
-
-        $("h2").each(function(index2) {
-            $(this).prepend((index1+1) + "." + (index2+1) + ". ");
-
-            $("h3").each(function(index3) {
-                $(this).prepend((index1+1) + "." + (index2+1) + '.' + (index3+1) + ". ");
-            });
-
-        });
-    });
-});
- */
+// $.getScript('/static/custom/auto_number.js');
